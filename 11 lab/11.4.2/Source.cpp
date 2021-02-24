ac494037eb06009a01c8305cad9237ad6a10ae74
d02c4 <73337425+d02c4@users.noreply.github.com>
2021-02-24 22:09:01 +0300

    Add files via upload

---
+++ 11 lab/11.4.2/Source.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <queue>
+
+
+using namespace std;
+
+
+void EnterSize(int* size)
+{
+    cout << "¬ведите размер стека: \t";
+    cin >> *size;
+    while (*size < 1)
+    {
+        cout << "¬веден некорректный размер стека!" << endl;
+        cout << "¬ведите размер стека: \t";
+        cin >> *size;
+    }
+}
+
+
+void InitQueue(int size, queue<int> &q)
+{
+       int a;
+       for (int i = 1; i <= size; i++)
+       {
+           cout << "¬ведите " << i << " элемент очереди:\t";
+           cin >> a;
+           q.push(a);
+       }
+}
+
+
+void PrintQueue(queue<int> q)
+{
+    cout << endl;
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+}
+
+void EditQueue(queue<int> &q)
+{
+    int count = 1;
+    
+    queue<int> qd;
+
+    while (!q.empty())
+    {
+        if (count % 2 != 0)
+        {
+            qd.push(q.front());
+        }
+        q.pop();
+        count++;
+    }
+    q = qd;
+}
+
+int main()
+{
+
+    setlocale(LC_ALL, "Ru");
+    int size;
+    EnterSize(&size);
+
+	queue<int> q;
+    InitQueue(size, q);
+
+    PrintQueue(q);
+    EditQueue(q);
+    PrintQueue(q);
+
+}
