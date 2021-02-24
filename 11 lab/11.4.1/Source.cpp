c094219ba13e0b24e1579c0acd172c59b747d803
d02c4 <73337425+d02c4@users.noreply.github.com>
2021-02-24 22:07:23 +0300

    Add files via upload

---
+++ 11 lab/11.4.1/Source.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+
+using namespace std;
+
+
+struct Queue
+{
+	int inf;
+	Queue* next;
+};
+
+void EnterSize(int* size)
+{
+	cout << "¬ведите размер очереди: \t";
+	cin >> (*size);
+
+	while ((*size) < 1)
+	{
+		cout << "¬веден неправильный размер очереди!\n";
+		cout << "¬ведите размер очереди: \t";
+		cin >> (*size);
+	}
+}
+
+Queue* InitQueue(Queue* &ptrFirst, int size)
+{
+	if (size == 0)
+	{
+		return NULL;
+	}
+	else
+	{
+		Queue* ptr = new Queue;
+		ptrFirst = ptr;
+		ptr->next = NULL;
+		cout << "¬ведите 1 элемент очереди:\t";
+		cin >> ptr->inf;
+		for (int i = 2; i <= size; i++)
+		{
+			Queue* h = new Queue;
+			cout << "¬ведите " << i << " элемент очереди:\t";
+			cin >> h->inf;
+			h->next = NULL;
+			ptr->next = h;
+			ptr = ptr->next;
+		}
+		
+	}
+	return ptrFirst;
+}
+
+void PrintQueue(Queue* ptrFirst, int size)
+{
+
+	cout << endl;
+	Queue* ptr = ptrFirst;
+	while(ptr != NULL)
+	{
+		cout << ptr->inf << " ";
+		ptr = ptr->next;
+	}
+	cout << endl;
+}
+
+
+void EditQueue(Queue* ptrFirst, int size)
+{
+	int count = 1;
+	Queue* ptr = ptrFirst;
+	Queue* old = ptr;
+	Queue* D;
+
+	while (ptr != NULL)
+	{
+		if (count % 2 == 0)
+		{
+			D = ptr;
+			ptr = ptr->next;
+			old->next = ptr;
+			delete D;
+		}
+		else
+		{
+			old = ptr;
+			ptr = ptr->next;
+		}
+		count++;
+	}
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+
+	int size;
+	EnterSize(&size);
+	Queue* ptrFirst;
+	ptrFirst = InitQueue(ptrFirst, size);
+	PrintQueue(ptrFirst, size);
+
+	EditQueue(ptrFirst, size);
+
+	PrintQueue(ptrFirst, size);
+
+}
