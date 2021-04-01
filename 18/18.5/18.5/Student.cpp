9dde4a3144c44ef0eca8e52d229097f025bc5057
d02c4 <73337425+d02c4@users.noreply.github.com>
2021-04-01 13:38:27 +0300

    Update Student.cpp

--- 18/18.5/18.5/Student.cpp
+++ 18/18.5/18.5/Student.cpp
@@ -1,32 +1,115 @@
-#pragma once 
-#include "PERSON.h"
-#include "Object.h"
+#include "Student.h"
 
-//класс STUDENT наследуется от класса PERSON 
-class STUDENT :public PERSON
+//конструктор без параметров 
+STUDENT::STUDENT() :PERSON()
 {
-public:
-	STUDENT();//конструктор без параметров 
+	mark = 2;
+}
 
-	~STUDENT();//деструктор 
+//дестрктор 
+STUDENT::~STUDENT()
+{
 
-	void Show();// функция для просмотра атрибутов класса с помощью указателя
+}
 
-	STUDENT(string, int, int, string);//конструктор с параметрами 
-	STUDENT(const STUDENT&);//конструктор копирования
+//конструктор с параметрами
+STUDENT::STUDENT(string M, int C, int mark, string subject) :PERSON(M, C)
+{
+	this->name = M;
+	this->age = C;
+	this->mark = mark;
+	this->subject = subject;
+}
 
-	string Get_subject() { return subject; }//модификатор 
-	void Set_subject(string);//селектор
+//конструктор копирования 
+STUDENT::STUDENT(const STUDENT& other)
+{
+	this->name = other.name;
+	this->age = other.age;
+	this->mark = other.mark;
+	this->subject = other.subject;
+}
 
-	int Get_mark() { return mark; }//модификатор 
-	void Set_mark(int);//селектор
+//модификатор
+void STUDENT::Set_mark(int)
+{
+	this->mark = mark;
+}
 
-	STUDENT& operator=(const STUDENT&);//операция присваивания
+void STUDENT::Set_subject(string)
+{
+	this->subject = subject;
+}
 
-	void Check_Mark(const STUDENT&);
-	friend istream& operator>>(istream& in, STUDENT& l);//операция ввода
-	friend ostream& operator<<(ostream& out, const STUDENT& l); //операция вывода 
-protected:
-	int mark;//поле оценки студента
-	string subject;//поле предмета
-};
+//оперция присваивания
+STUDENT& STUDENT::operator=(const STUDENT& l)
+{
+	if (&l == this)return *this;
+	name = l.name;
+	age = l.age;
+	mark = l.mark;
+	subject = l.subject;
+	return *this;
+}
+
+//операция ввода
+istream& operator>>(istream& in, STUDENT& l)
+{
+	cout << "\nName:";
+	in >> l.name;
+
+	cout << "\nAge:";
+	in >> l.age;
+	while (l.age < 14 || l.age > 40)
+	{
+		cout << "\nНе уверен что в таком возрасте учатся в университете\n";
+		cout << "\nAge:";
+		in >> l.age;
+	}
+
+	cout << "\nsubject:";
+	in >> l.subject;
+
+	cout << "\nmark:";
+	in >> l.mark;
+	while (l.mark < 2 || l.mark > 5)
+	{
+		cout << "\nВведена некорректная оценка!\n";
+		cout << "\nMark:";
+		in >> l.mark;
+	}
+	return in;
+}
+
+//операция вывода
+ostream& operator<<(ostream& out, const STUDENT& l)
+{
+	out << "\nName : " << l.name;
+	out << "\nAge : " << l.age;
+	out << "\nmark : " << l.mark;
+	out << "\nsubject:" << l.subject;
+	out << "\n";
+	return out;
+}
+
+void STUDENT::Check_Mark(const STUDENT& stud)
+{
+	switch (stud.mark)
+	{
+	case 2: cout << "\nОценка  студента " << stud.name << " по предмету " << stud.subject << " неудовлетворительна!\n"; break;
+	case 3:
+	case 4:
+	case 5: cout << "\nОценка  студента " << stud.name << " по предмету " << stud.subject << " удовлетворительна!\n"; break;
+	default:
+		break;
+	}
+}
+
+void STUDENT::Show()
+{
+	cout << "\nName : " << name << endl;
+	cout << "\nAge : " << age << endl;
+	cout << "\nMark : " << mark << endl;
+	cout << "\nSubject : " << subject << endl;
+	Check_Mark(*this);
+}
