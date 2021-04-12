c9d2d5b5ed9221dbe6a782afcff2c3b61c3a55a6
d02c4 <73337425+d02c4@users.noreply.github.com>
2021-04-12 13:31:57 +0300

    Update 18.7.cpp

--- 18/18.7/18.7/18.7.cpp
+++ 18/18.7/18.7/18.7.cpp
@@ -7,9 +7,9 @@ int main()
 	setlocale(LC_ALL, "Ru");
 	LIST<double> list1(10);
 	LIST<double> list2(5);
-	cout << "Заполните первый LIST\n";
+	cout << "Р—Р°РїРѕР»РЅРёС‚Рµ РїРµСЂРІС‹Р№ LIST\n";
 	cin >> list1;
-	cout << "Заполните второй LIST\n";
+	cout << "Р—Р°РїРѕР»РЅРёС‚Рµ РІС‚РѕСЂРѕР№ LIST\n";
 	cin >> list2;
 	cout << "___________________________________________________________\n";
 	cout << "LIST 1:\t" << list1 << endl;
@@ -19,6 +19,6 @@ int main()
 	LIST<double> list3 = list1 * list2;
 	cout << "LIST1 * LIST2 = \t" << list3 << endl;
 	LIST<Pair> list4(5);
-	cout << "Список пар чисел:\n" << list4 << endl;
+	cout << "РЎРїРёСЃРѕРє РїР°СЂ С‡РёСЃРµР»:\n" << list4 << endl;
 	return 0;
 }
