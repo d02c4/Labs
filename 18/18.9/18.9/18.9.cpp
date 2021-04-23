ab2935eecc4eb25edcb5c194a0ff16223d2c5866
d02c4 <d02c44@gmail.com>
2021-04-23 23:07:26 +0300

    add  lab 18.9

---
+++ 18/18.9/18.9/18.9.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include "Iterator.h"
+#include "LIST.h"
+#include "Error.h"
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	try
+	{
+		int size = 0;
+		cout << "Введите размер первого LIST: ";
+		cin >> size;
+		LIST list1(size);
+		cout << "Введите размер второго LIST: ";
+		cin >> size;
+		LIST list2(size);
+		cout << "Заполните первый LIST\n";
+		cin >> list1;
+		cout << "Заполните второй LIST\n";
+		cin >> list2;
+		cout << "___________________________________________________________\n";
+		cout << "LIST 1:\t" << list1 << endl;
+		cout << "___________________________________________________________\n";
+		cout << "LIST 2:\t" << list2 << endl;
+		cout << "___________________________________________________________\n";
+		LIST list3 = list1 * list2;
+		cout << "LIST1 * LIST2 = \t" << list3 << endl;
+		Iterator tmp;
+		tmp(&list1[0]);
+		tmp = tmp + 2;
+		cout << "___________________________________________________________\n";
+		cout << *tmp << endl;
+		cout << "___________________________________________________________\n";
+	}
+	catch (Error &ex)
+	{
+		ex.what();
+	}
+	return 0;
+}
---
+++ 18/18.9/18.9/LIST.h
@@ -0,0 +1,48 @@
+#pragma once 
+#include <string> 
+#include <iostream> 
+#include "Iterator.h"
+using namespace std;
+
+const int MAX_SIZE = 20;
+
+class LIST
+{
+public:
+	LIST();
+	LIST(int s); // Конструктор с параметрами
+	LIST(const LIST& a); // Конструктор с параметрами
+	~LIST(); // деструктор
+	LIST& operator = (const LIST& a);// оператор присваивания
+	int& operator [] (int index); // операция доступа к индексу
+	LIST& operator * (LIST& a);
+	LIST& operator +(int n);
+	int operator ()();
+	void SetData(int); // Сеттер данных ячейки вектора
+	void SetSize(int); // Сеттер размера вектора
+	int GetData(); // Геттер данных ячейчки
+	int GetSize(); //Геттер размера вектора
+	Iterator begin()
+	{
+		return Begin;
+	}
+	Iterator end()
+	{
+		return End;
+	}
+	int size()
+	{
+		return Size;
+	}
+
+	friend ostream& operator<<(ostream& out, const LIST& a);
+	friend istream& operator>>(istream& in, LIST& a);
+
+private:
+	friend class Iterator;
+	int Size = 0;
+	int* data;
+	Iterator Current;
+	Iterator Begin;
+	Iterator End;
+};
